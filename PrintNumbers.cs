595d94dc5b6808fd3dd75acd3765ea7c49fd9faf
InaMil <ilina.mileva.1@gmail.com>
2013-08-02 22:24:53 +0300

    Create PrintNumbers.cs

---
+++ PrintNumbers.cs
@@ -0,0 +1,11 @@
+// 4. Write a program to print the numbers 1, 101 and 1001
+
+using System;
+
+class PrintNumbers
+{
+    static void Main()
+    {
+        Console.WriteLine("1" + Environment.NewLine + "101" + Environment.NewLine + "1001");
+    }
+}
