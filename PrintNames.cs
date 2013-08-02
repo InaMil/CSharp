166a716b0bcd1286d9d48301153973b482a37ec5
InaMil <ilina.mileva.1@gmail.com>
2013-08-02 22:25:52 +0300

    Create PrintNames.cs

---
+++ PrintNames.cs
@@ -0,0 +1,13 @@
+// 6. Create console application that prints your first and last name
+
+using System;
+
+class PrintNames
+{
+    static void Main()
+    {
+        string firstName = "Petar";
+        string lastName = "Petrov";
+        Console.WriteLine("{0} {1}", firstName, lastName);
+    }
+}
